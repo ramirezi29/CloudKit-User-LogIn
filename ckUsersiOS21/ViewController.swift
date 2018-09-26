81ae69d08534eeacb2053c68a63fc8202fdda166
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2018-09-26 09:58:57 -0600

    Initial Commit

---
+++ ckUsersiOS21/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ckUsersiOS21
+//
+//  Created by Ivan Ramirez on 9/26/18.
+//  Copyright © 2018 ramcomw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
